cbb139eaa2225b01c2a0544841dda6a1b537afe0
DZhang3210 <davidzhang3210@gmail.com>
2024-09-06 21:32:07 -0400

    new commit

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -49,7 +49,7 @@ const WorkspaceIdPage = () => {
     workspaceId,
   ]);
 
-  if (workspaceLoading || channelsLoading) {
+  if (workspaceLoading || channelsLoading || memberLoading) {
     return (
       <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
         <Loader className="size-6 animate-spin text-muted-foreground" />
