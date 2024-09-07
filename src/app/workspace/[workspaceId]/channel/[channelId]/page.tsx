2d9b0b16d5dfa0f586448ab2426c4616d26ea4f6
DZhang3210 <davidzhang3210@gmail.com>
2024-09-07 13:20:21 -0400

    new commit

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,14 +1,40 @@
 "use client";
+import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-workspace-modal";
+import useChannelId from "@/hooks/use-channel-id";
 import useWorkspaceId from "@/hooks/use-workspace-id";
+import { Loader, TriangleAlert } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Header from "./header";
 
 const ChannelIdPage = () => {
-  const router = useRouter();
-  const workspaceId = useWorkspaceId();
-  const [_open, setOpen] = useCreateChannelModal();
-  return <div></div>;
+  const channelId = useChannelId();
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
+
+  if (channelLoading) {
+    return (
+      <div className="h-screen flex-1 flex items-center justify-center">
+        <Loader className="animate-spin size-5 text-muted-foreground" />
+      </div>
+    );
+  }
+  if (!channel) {
+    return (
+      <div className="h-screen flex-1 flex-col gap-y-2 flex items-center justify-center">
+        <TriangleAlert className="size-5 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel not found</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header title={channel.name} />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
