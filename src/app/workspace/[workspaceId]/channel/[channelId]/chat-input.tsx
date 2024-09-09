6f75fc0bc9b43dc49d9ff9cd76b152f3fb19017c
DZhang3210 <davidzhang3210@gmail.com>
2024-09-09 16:01:41 -0400

    new commit

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -10,12 +10,23 @@ interface ChatInputProps {
 
 const ChatInput = ({ placeholder }: ChatInputProps) => {
   const editorRef = useRef<Quill | null>(null);
+
+  const handleSubmit = ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    console.log({ body, image });
+  };
+
   return (
     <div className="px-5 w-full">
       <Editor
         variant="create"
         placeholder={placeholder}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         disabled={false}
         innerRef={editorRef}
       />
--- src/components/ui/editor.tsx
+++ src/components/ui/editor.tsx
@@ -82,7 +82,19 @@ const Editor = ({
             enter: {
               key: "Enter",
               handler: () => {
-                //TODO Submit form
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) return;
+
+                const body = JSON.stringify(quill.getContents());
+
+                submitRef.current?.({ body, image: addedImage });
+
                 return;
               },
             },
@@ -141,7 +153,7 @@ const Editor = ({
     quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   console.log({ isEmpty, text });
 
@@ -219,14 +231,19 @@ const Editor = ({
               <Button
                 variant={"outline"}
                 size="sm"
-                onClick={() => {}}
+                onClick={onCancel}
                 disabled={disabled}
               >
                 Cancel
               </Button>
               <Button
                 size="sm"
-                onClick={() => {}}
+                onClick={() =>
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  })
+                }
                 disabled={disabled || isEmpty}
                 className="bg-[#007a5a] hover:bg-[007a5a]/80 text-white"
               >
@@ -237,7 +254,12 @@ const Editor = ({
           {variant === "create" && (
             <Button
               disabled={disabled || isEmpty}
-              onClick={() => {}}
+              onClick={() =>
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                })
+              }
               className={cn(
                 "ml-auto",
                 isEmpty
